60844fde40d50b81eaa0d68f8bd807153a1d46a8
nabendu82 <nabendu.biswas@gmail.com>
2023-04-19 08:41:08 +0530

    JS -1

---
+++ JSbasics/main.js
@@ -0,0 +1,30 @@
+// console.log("Hello JavaScript");
+
+//Variables
+// var firstName = 'Nabendu';
+// var age = 40;
+// var rating = 5.5;
+// var isDeveloper = true;
+
+// const firstAge = 8;
+// firstAge = 10;
+
+// console.log(firstName);
+// console.log(age);
+// console.log(rating);
+// console.log(isDeveloper);
+
+//Datatypes - String, Numbers, Boolean, null, undefined
+const firstName = 'Nabendu';
+const age = 40;
+const rating = 5.5;
+const isDeveloper = true;
+const x = null;
+let y;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isDeveloper);
+console.log(typeof x);
+console.log(typeof y);
