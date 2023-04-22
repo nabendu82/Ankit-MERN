abf41906b73ee0b6ac81fc68bcb698141839868e
nabendu82 <nabendu.biswas@gmail.com>
2023-04-22 08:35:53 +0530

    JS -2

--- JSbasics/main.js
+++ JSbasics/main.js
@@ -15,16 +15,37 @@
 // console.log(isDeveloper);
 
 //Datatypes - String, Numbers, Boolean, null, undefined
-const firstName = 'Nabendu';
-const age = 40;
-const rating = 5.5;
-const isDeveloper = true;
-const x = null;
-let y;
+// const firstName = 'Nabendu';
+// const age = 40;
+// const rating = 5.5;
+// const isDeveloper = true;
+// const x = null;
+// let y;
 
-console.log(typeof firstName);
-console.log(typeof age);
-console.log(typeof rating);
-console.log(typeof isDeveloper);
-console.log(typeof x);
-console.log(typeof y);
+// console.log(typeof firstName);
+// console.log(typeof age);
+// console.log(typeof rating);
+// console.log(typeof isDeveloper);
+// console.log(typeof x);
+// console.log(typeof y);
+
+//Difference between var and let
+function varFn(){
+    if(true){
+        var num1 = 10;
+        console.log("Inside if var " + num1);
+    }
+    
+    console.log("Outside if var " + num1);
+}
+
+varFn()
+console.log("Outside function var " + num1);
+
+
+// if(true){
+//     let num2 = 20;
+//     console.log("Inside if let " + num2);
+// }
+
+// console.log("Outside if let " + num2);
