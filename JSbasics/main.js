b9cd082d465748562ea4d895aed5b12e9c6ae055
nabendu82 <nabendu.biswas@gmail.com>
2023-05-02 08:45:53 +0530

    JS Basics-3

--- JSbasics/main.js
+++ JSbasics/main.js
@@ -184,29 +184,101 @@ const lang = [
 //     console.log('a is greater then 5')
 // }
 
-const x = '10';
-if(x == 10){//10 == 10
-    console.log('x is 10');
-}
+// const x = '10';
+// if(x == 10){//10 == 10
+//     console.log('x is 10');
+// }
 
 //if-else statement
-const y = '20';
-if(y === 20){
-    console.log('y is 20')
-} else {
-    console.log('y is string 20')
-}
+// const y = '20';
+// if(y === 20){
+//     console.log('y is 20')
+// } else {
+//     console.log('y is string 20')
+// }
 
 //if-else if-else statement
-const z = 79;
-if(z>100){
-    console.log('z is greater then 100')
-} else if(z>80){
-    console.log('z is greater then 80')
-} else if(z>60){
-    console.log('z is greater then 60')
-} else if(z>40){
-    console.log('z is greater then 40')
-} else {
-    console.log('z is lesser then 40')
+// const z = 79;
+// if(z>100){
+//     console.log('z is greater then 100')
+// } else if(z>80){
+//     console.log('z is greater then 80')
+// } else if(z>60){
+//     console.log('z is greater then 60')
+// } else if(z>40){
+//     console.log('z is greater then 40')
+// } else {
+//     console.log('z is lesser then 40')
+// }
+
+//OR and AND operators
+// const x = 20;
+// const y = 30;
+
+// if(x === 20 || y === 40){
+//     console.log('Any one of the statement is true');
+// }
+
+// if(x === 20 && y === 30){
+//     console.log('Both of the statement are true');
+// }
+
+//Ternary operators
+// const z = 10;
+// let color;
+// if(z > 10){
+//     color = 'red'
+// } else {
+//     color = 'blue'
+// }
+
+// console.log(color);
+
+// const color2 = z > 9 ? 'red' : 'blue';
+// console.log(color2)
+
+//Switch Statement
+// const color = 'blue';
+
+// switch(color){
+//     case 'red':
+//         console.log('Color is red');
+//         break;
+//     case 'green':
+//         console.log('Color is green');
+//         break;
+//     case 'blue':
+//     case 'yellow':
+//         console.log('Color is blue or yellow');
+//         break;
+//     default:
+//         console.log('No color was matched');
+// }
+
+//Functions
+
+//Method #1 - Function decleration
+function addNums(num1, num2){
+    console.log(num1 + num2);
 }
+
+addNums(30, 20);
+
+//Method #2 - Function expressions
+const multiNums = function(num1, num2){
+    return num1 * num2;
+}
+
+console.log(multiNums(30, 20))
+
+//Method #3 - Arrow Function
+const modNums = (num1, num2) => {
+    return num1 % num2;
+}
+
+console.log(modNums(30, 20))
+
+//Method #4 - Condensed Arrow Function
+const divNums = num1 =>  num1 / 2;
+
+console.log(divNums(30))
