dc76ba4832559ccf3e3e114972de7141e0799e78
nabendu82 <nabendu.biswas@gmail.com>
2023-05-24 08:34:12 +0530

    ES6-2

--- ES6/main.js
+++ ES6/main.js
@@ -5,13 +5,13 @@ const firstAge = 6;
 const childAges = [5, 6, 8, 10];
 // childAges = [5, 6, 8, 10, 12];
 childAges.push(12);
-console.log(childAges);
+// console.log(childAges);
 
 const personObj = {name: 'Nabendu', skill: 'JS'}
 personObj.skill = 'React';
 personObj.profession = 'Trainer';
 
-console.log(personObj);
+// console.log(personObj);
 
 //Default parameters
 const isEqual = (num1, num2=10) => {
@@ -20,8 +20,8 @@ const isEqual = (num1, num2=10) => {
     return num1 === num2;
 }
 
-console.log(isEqual(10, 20));
-console.log(isEqual(10));
+// console.log(isEqual(10, 20));
+// console.log(isEqual(10));
 
 //Object Literals Extensions
 const name = 'Nabendu';
@@ -32,7 +32,7 @@ let obj = {
     age
 }
 
-console.log(obj);
+// console.log(obj);
 
 //Dynamic Fields
 let ageField = 'age'
@@ -42,7 +42,7 @@ const es6Obj = {
     [ageField]: 40
 }
 
-console.log(es6Obj)
+// console.log(es6Obj)
 
 //Rest Opearators
 let arrFunc = (...arr) => {
@@ -56,3 +56,59 @@ let restFunc = (a, b, c, ...n) => {
 }
 
 restFunc(55, 67, 12, 32, 45, 78, 12);
+
+//Spread Operator
+let a = [55, 67, 12, 32, 45, 78, 12];
+console.log(Math.min(2, 3, 1));
+console.log(Math.min(...a));
+console.log(Math.max(...a));
+
+//Copy by reference problem
+let x = [67, 12, 32, 45];
+let y = x;
+y.push(55);
+// console.log(x)
+// console.log(y)
+
+//Solution with spread
+let c = [55, 67, 12, 32];
+let d = [...c];
+d.push(13);
+// console.log(c);
+// console.log(d);
+
+//Spread operator to merge array
+let x1 = [55, 67], x2 = [12, 32], x3 = [78, 99];
+let d1 = [...x1, ...x2, ...x3];
+// console.log(d1)
+
+//Spread operator in Objects
+let obj1 = { foo: 'bar', x: 42 }, obj2 = { foo: 'baz', y: 13}
+let clonedObj = {...obj1}
+console.log(clonedObj)
+let mergedObj = {...obj1, ...obj2}
+console.log(mergedObj)
+
+//Array Destructuring
+const rgb = [255, 200, 0];
+// const r = rgb[0]
+// const g = rgb[1]
+// const b = rgb[2]
+
+const [r, g, b] = rgb;
+console.log(r, g, b);
+
+//Object destructuring
+const developer = {
+    firstname: 'Nabendu',
+    lastname: 'Biswas',
+    country: 'India'
+};
+
+// const firstname = developer.firstname;
+// const lastname = developer.lastname;
+// const country = developer.country;
+
+const {firstname, lastname, country} = developer;
+console.log(firstname, lastname, country)
+  
