c792e687c3c2134927baac27f8e3c2ed46611417
nabendu82 <nabendu.biswas@gmail.com>
2023-05-25 08:43:50 +0530

    ES6 done

--- ES6/main.js
+++ ES6/main.js
@@ -87,7 +87,7 @@ let obj1 = { foo: 'bar', x: 42 }, obj2 = { foo: 'baz', y: 13}
 let clonedObj = {...obj1}
 console.log(clonedObj)
 let mergedObj = {...obj1, ...obj2}
-console.log(mergedObj)
+// console.log(mergedObj)
 
 //Array Destructuring
 const rgb = [255, 200, 0];
@@ -96,7 +96,7 @@ const rgb = [255, 200, 0];
 // const b = rgb[2]
 
 const [r, g, b] = rgb;
-console.log(r, g, b);
+// console.log(r, g, b);
 
 //Object destructuring
 const developer = {
@@ -110,5 +110,105 @@ const developer = {
 // const country = developer.country;
 
 const {firstname, lastname, country} = developer;
-console.log(firstname, lastname, country)
+// console.log(firstname, lastname, country)
+
+//ES6 classes
+class CreateRoom{
+    constructor(name){
+        this.room = `${name}'s room`
+    }
+
+    cleanRoom(soap){
+        console.log(`Cleaning ${this.room} with ${soap}`)
+    }
+}
+
+const nabRoom = new CreateRoom('Nabendu'); 
+const shikhaRoom = new CreateRoom('Shikha'); 
+
+// nabRoom.cleanRoom('Domex');
+// shikhaRoom.cleanRoom('Lizol');
+
+//Classical for loop
+let languages = ['Java', 'JavaScript', 'Cobol'];
+for(let i=0; i<languages.length; i++){
+    // console.log(languages[i])
+}
+  
+//forEach
+// languages.forEach(lang => console.log(lang));
+
+//Map use case
+let numbers = [6, 8, 10];
+
+let doubledNums = [];
+for(let i=0; i<numbers.length; i++){
+    doubledNums.push(numbers[i]*2)
+}
+// console.log(doubledNums)
+
+//map
+let doubled = numbers.map(number => number*2);
+console.log(doubled)
+
+//filter use case
+let products = [
+    { name: 'Samsung M21', category: 'smartphone'},
+    { name: 'Samsung M31', category: 'smartphone'},
+    { name: 'Macbook Air', category: 'laptop'},
+    { name: 'Asus Zenbook', category: 'laptop'}
+];
+
+let filteredProducts = [];
+for(let i=0; i<products.length; i++){
+    if(products[i].category === 'smartphone'){
+        filteredProducts.push(products[i])
+    }
+}
+// console.log(filteredProducts)
+
+//filter
+let filtered = products.filter(item => item.category === 'laptop');
+console.log(filtered)
+
+//find use case
+let users = [
+    {name: 'Nabendu'},
+    {name: 'Shikha'},
+    {name: 'Hriday'},
+    {name: 'Robin'},
+    {name: 'Mousam'},
+    {name: 'Hari'}
+];
+
+let user;
+for(let i=0; i<users.length; i++){
+    if(users[i].name === 'Shikha'){
+        user = users[i];
+        break;
+    }
+}
+console.log(user)
+
+//find
+const coder = users.find(user => user.name === 'Mousam');
+console.log(coder)
+
+//some and every
+let devices = [
+    { name: 'Samsung M21', category: 'smartphone', ram: 4},
+    { name: 'Samsung M31', category: 'smartphone', ram: 6},
+    { name: 'Macbook Air', category: 'laptop', ram: 8},
+    { name: 'Asus Zenbook', category: 'laptop', ram: 16}
+];
+
+//every
+const allDevCanCode = devices.every(device => device.ram >= 6);
+console.log(allDevCanCode)
+
+//some
+const someDevCanCode = devices.some(device => device.ram >= 6);
+console.log(someDevCanCode)
+  
+
   
