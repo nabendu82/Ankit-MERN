4424ef0793cf7286e3e83029cf75665061c19eec
nabendu82 <nabendu.biswas@gmail.com>
2023-07-22 07:40:09 +0530

    contact app done

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
 import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ContactDetail from './components/ContactDetail';
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
@@ -10,6 +12,11 @@ function App() {
 
   const addContactHandler = contact => {
     setContacts([...contacts, { id: contacts.length, ...contact }])
+  }
+
+  const removeContactHandler = id => {
+    const newContactList = contacts.filter(contact => contact.id !== id);
+    setContacts(newContactList);
   }
 
   useEffect(() => {
@@ -23,9 +30,14 @@ function App() {
 
   return (
     <div className='ui container'>
-      <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={() => <ContactList contacts={contacts} getContactId={removeContactHandler} />} />
+          <Route path="/add" component={props => <AddContact {...props}addContactHandler={addContactHandler} />}/>
+          <Route path="/contact/:id" component={ContactDetail} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- contact-app/src/components/ContactCard.js
+++ contact-app/src/components/ContactCard.js
@@ -1,16 +1,19 @@
 import user from "../images/user.png";
+import { Link } from "react-router-dom"
 
 const ContactCard = props => {
-    const { name, email } = props.contact;
+    const { id, name, email } = props.contact;
 
     return (
         <div className="item">
             <img src={user} alt="user" className="ui avatar image" />
             <div className="content">
-                <div className="header">{name}</div>
-                <div>{email}</div>
+                <Link to={{ pathname: `/contact/${id}`}}>
+                    <div className="header">{name}</div>
+                    <div>{email}</div>
+                </Link>
             </div>
-            <i className="trash alternate outline icon" style={{ color: "red" }}></i>
+            <i className="trash alternate outline icon" style={{ color: "red" }} onClick={() => props.clickHandler(id)}></i>
         </div>
     )
 }
