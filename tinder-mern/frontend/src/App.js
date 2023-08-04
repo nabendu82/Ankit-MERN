19caf5819adaa38f876f6ae8b1b5f3458d818d1e
nabendu82 <nabendu.biswas@gmail.com>
2023-08-04 07:57:20 +0530

    Final commit

--- tinder-mern/frontend/src/App.js
+++ tinder-mern/frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+import TinderCards from './components/TinderCards';
 
 function App() {
   return (
     <div className="app">
-      <h1>Tinder clone</h1>
+      <Header />
+      <TinderCards />
     </div>
   );
 }
